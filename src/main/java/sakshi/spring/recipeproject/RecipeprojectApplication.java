a43debcd0c6a87668c3672b6ec2b3b5bb7138733
Sakshi Singhal <sakshi.igdtu@gmail.com>
2019-03-31 10:51:21 +0530

    first commit

---
+++ src/main/java/sakshi/spring/recipeproject/RecipeprojectApplication.java
@@ -0,0 +1,13 @@
+package sakshi.spring.recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeprojectApplication.class, args);
+    }
+
+}
